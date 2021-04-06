427c550c08c45562c2f6aa68dbfbc3f80d1f1d27
Lester Manuel <lester.manuel@mynt.xyz>
2021-04-06 14:11:04 +0800

    added postmapping of activity and account

---
+++ d1a1-master/account-management/src/main/java/ph/apper/account/App.java
@@ -0,0 +1,34 @@
+package ph.apper.account;
+
+import lombok.Data;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @RestController
+    @RequestMapping("activity")
+    public static class ActivityController {
+
+        @PostMapping
+        public ResponseEntity create(@RequestBody Request request) {
+            System.out.println(request);
+
+            return ResponseEntity.ok().build();
+        }
+    }
+
+    @Data
+    public static class Request {
+        private String action;
+        private String identifier;
+    }
+}
+
+
